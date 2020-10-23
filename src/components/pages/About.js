4072ec2aac0f36dd7e6294767e4ca19251c3f747
Jean-Marc DIOT <jean.marc.diot.mail@gmail.com>
2020-10-23 16:20:25 +0200

    Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the TodoList app. <br />
+        Following the tutorial React Crash Course by Traversy Media
+      </p>
+    </React.Fragment>
+  )
+}
